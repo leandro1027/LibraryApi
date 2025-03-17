e645debbf173b7587116750e01209cbf579ca1d2
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-17 10:47:59 -0300

    Update Autor.java

--- src/main/java/com/example/LibraryApi/model/Autor.java
+++ src/main/java/com/example/LibraryApi/model/Autor.java
@@ -1,4 +1,30 @@
 package com.example.LibraryApi.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name= "Autor", schema = "public")
+@Getter
+@Setter
 public class Autor {
+
+    @Id
+    @Column(name = "id", length = 100, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nascionalidade", length = 50, nullable = false)
+    private String nascionalidade;
+
 }
