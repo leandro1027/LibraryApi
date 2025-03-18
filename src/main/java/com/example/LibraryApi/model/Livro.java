7c574e74ff47318108dca3ed428cacf29661e03c
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-18 09:37:55 -0300

    Update Livro.java

--- src/main/java/com/example/LibraryApi/model/Livro.java
+++ src/main/java/com/example/LibraryApi/model/Livro.java
@@ -1,4 +1,24 @@
 package com.example.LibraryApi.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
 }
