dd6aeaeaa3bfa96d456f2a6838cbbd4dc7fcd8d1
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-21 11:06:08 -0300

    Update LibraryApiApplication.java

--- src/main/java/com/example/LibraryApi/LibraryApiApplication.java
+++ src/main/java/com/example/LibraryApi/LibraryApiApplication.java
@@ -1,7 +1,11 @@
 package com.example.LibraryApi;
 
+import com.example.LibraryApi.model.Autor;
+import com.example.LibraryApi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LibraryApiApplication {
@@ -10,6 +14,17 @@ public class LibraryApiApplication {
 		SpringApplication.run(LibraryApiApplication.class, args);
 	}
 
+	public static  void exemploSalvar (AutorRepository autorRepository) {
+		Autor autor = new Autor();
+		autor.setNome("Leandro");
+		autor.setNascionalidade("Brasileiro");
+		autor.setDataNascimento(LocalDate.of(2004, 8, 9));
+		autorRepository.save(autor);
+	}
+
+
 }
+
+
 
 
