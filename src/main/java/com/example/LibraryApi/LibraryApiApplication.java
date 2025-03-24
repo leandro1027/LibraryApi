c52fb1995d417f10d472bac1d015bb1dfb1e67c7
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-24 11:01:34 -0300

    Update LibraryApiApplication.java

--- src/main/java/com/example/LibraryApi/LibraryApiApplication.java
+++ src/main/java/com/example/LibraryApi/LibraryApiApplication.java
@@ -1,12 +1,15 @@
 package com.example.LibraryApi;
 
 import com.example.LibraryApi.model.Autor;
+import com.example.LibraryApi.model.GeneroLivro;
+import com.example.LibraryApi.model.Livro;
 import com.example.LibraryApi.repository.AutorRepository;
 import com.example.LibraryApi.repository.LivroRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @SpringBootApplication
 public class LibraryApiApplication {
@@ -17,7 +20,7 @@ public class LibraryApiApplication {
 		LivroRepository livroRepository = context.getBean(LivroRepository.class);
 
 		exemploSalvarAutor(autorRepository);
-		exemploSalvarLivro(livroRepository);
+		exemploSalvarLivro(livroRepository, autorRepository);
 	}
 
 	public static  void exemploSalvarAutor (AutorRepository autorRepository) {
@@ -31,7 +34,19 @@ public class LibraryApiApplication {
 
 	}
 
-	public  static  void exemploSalvarLivro(LivroRepository livroRepository) {}
+	public static void exemploSalvarLivro(LivroRepository livroRepository, AutorRepository autorRepository) {
+		// Buscar um autor existente
+		Autor autor = autorRepository.findById(UUID.fromString("ebfaaec3-2f7e-4dde-8bf0-6702c225147e")).orElse(null);
+
+		Livro livro = new Livro();
+		livro.setAutor(autor);
+		livro.setGenero(GeneroLivro.ANIMAÇÃO);
+		livro.setDataPublicacao(LocalDate.of(2025, 3, 24));
+
+		var livroSalvo = livroRepository.save(livro);
+		System.out.println("Livro salvo: " + livroSalvo);
+	}
+
 
 
 }
