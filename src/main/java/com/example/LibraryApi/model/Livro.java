9d629a4dd98ac413c367e8d9b2f75aeac122b04c
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-25 10:32:30 -0300

    Update Livro.java

--- src/main/java/com/example/LibraryApi/model/Livro.java
+++ src/main/java/com/example/LibraryApi/model/Livro.java
@@ -1,21 +1,27 @@
 package com.example.LibraryApi.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "livro")
+@Data
 public class Livro {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
 
     @Column(name = "data_publicacao")
     private LocalDate dataPublicacao;
@@ -27,7 +33,7 @@ public class Livro {
     @Column(name = "preco", precision = 18, scale = 2)
     private BigDecimal preco;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_autor")
     private Autor autor;
 }
